9b3a2463fe19fcf96ff7d6a46f1006974d15a799
Runar Heggset <rukki093@gmail.com>
2023-02-14 10:31:35 +0100

    feat(linux): add option to connect to hidden wifi

--- src/linux-connect.js
+++ src/linux-connect.js
@@ -17,6 +17,11 @@ function connectToWifi(config, ap, callback) {
     args.push(config.iface);
   }
 
+  if (ap.hidden) {
+    args.push('hidden');
+    args.push('yes');
+  }
+
   execFile('nmcli', args, { env }, (err, resp) => {
     // Errors from nmcli came from stdout, we test presence of 'Error: ' string
     if (resp.includes('Error: ')) {
